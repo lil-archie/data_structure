1218c1b610e0a6bae5a5c6d655ce32d4a888b661
lil-archie <108964143+lil-archie@users.noreply.github.com>
2023-03-14 21:52:34 +0800

    Add files via upload

---
+++ test_3_14/test.cpp
@@ -0,0 +1,23 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+    return 0;
+}
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int A, B, C, D;
+    cin >> A >> B >> C >> D;
+    cout << "DIFERENCA = " << (A * B) - (C * D) << endl;
+}
+
