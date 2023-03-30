8118006a472f4f5f28252d6971fcebc4938bdf3c
lil-archie <108964143+lil-archie@users.noreply.github.com>
2023-03-30 20:30:52 +0800

    Add files via upload

---
+++ test_3_30/test.c
@@ -0,0 +1,105 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+
+#include"Sort.h"
+
+void PrintArray(int* a, int n)
+{
+	for (int i = 0; i < n; ++i)
+	{
+		printf("%d ", a[i]);
+	}
+	printf("\n");
+}
+
+// 升序
+// 最坏：O(N^2)  逆序
+// 最好：O(N)    顺序有序
+void InsertSort(int* a, int n)
+{
+	for (int i = 1; i < n; i++)
+	{
+		int end = i - 1;
+		int tmp = a[i];
+		// 将tmp插入到[0,end]区间中，保持有序
+		while (end >= 0)
+		{
+			if (tmp < a[end])
+			{
+				a[end + 1] = a[end];
+				--end;
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		a[end + 1] = tmp;
+	}
+}
+
+void ShellSort(int* a, int n)
+{
+	/*int gap = 3;
+	for (int j = 0; j < gap; j++)
+	{
+		for (int i = j; i < n - gap; i += gap)
+		{
+			int end = i;
+			int tmp = a[i + gap];
+			while (end >= 0)
+			{
+				if (tmp < a[end])
+				{
+					a[end + gap] = a[end];
+					end -= gap;
+				}
+				else
+				{
+					break;
+				}
+			}
+			a[end + gap] = tmp;
+		}
+	}*/
+
+	// gap > 1 预排序
+	// gap == 1 直接插入排序
+	int gap = n;
+	while (gap > 1)
+	{
+		//gap /= 2;
+		gap = gap / 3 + 1;
+
+		for (int i = 0; i < n - gap; i++)
+		{
+			int end = i;
+			int tmp = a[i + gap];
+			while (end >= 0)
+			{
+				if (tmp < a[end])
+				{
+					a[end + gap] = a[end];
+					end -= gap;
+				}
+				else
+				{
+					break;
+				}
+			}
+			a[end + gap] = tmp;
+		}
+
+		//PrintArray(a, n);
+	}
+}
+
+
+#pragma once
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+void PrintArray(int* a, int n);
+void InsertSort(int* a, int n);
+void ShellSort(int* a, int n);
